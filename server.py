bd89db52ab636d0f303f3f3814a2896561ae23ce
Axiles30 <stroykomp01@gmail.com>
2023-03-03 22:44:04 +0400

    2

--- server.py
+++ server.py
@@ -7,47 +7,52 @@ from aiohttp import web
 
 
 def load_quotes() :
-  with open('quotes.json', 'r', encoding='utf-8') as f :
-    data = json.load(f)
-    return data
+	with open('quotes.json', 'r', encoding='utf-8') as f :
+		data = json.load(f)
+		return data
 
 
 async def index(request) :
-  with open("./templates/index.html", 'r', encoding='utf-8') as f:
-    return web.Response(text=f.read(), content_type='text/html')
+	with open("./templates/index.html", 'r', encoding='utf-8') as f :
+		return web.Response(text=f.read(), content_type='text/html')
+
+
+async def send_random_quote(quotes, web_socket):
+	while True:
+		_, quote = random.choice(list(quotes.items()))
+		await web_socket.send_str(quote)
+		await asyncio.sleep(10)
 
 
 async def websocket_handler(request) :
-  web_socket = web.WebSocketResponse()
-  await web_socket.prepare(request)
-  quotes = load_quotes()
-  subscribe = True
+	web_socket = web.WebSocketResponse()
+	await web_socket.prepare(request)
+	quotes = load_quotes()
+	loop = asyncio.get_event_loop()
 
-  while subscribe :
-    await asyncio.sleep(10)
-    id, quote = random.choice(list(quotes.items()))
-    await web_socket.send_str(quote)
+	task = None
 
-  async for msg in web_socket:
-    if msg.type == aiohttp.WSMsgType.TEXT:
-      if msg.data == 'close':
-        await web_socket.close()
-      else:
-        await web_socket.send_str(msg.data + '/answer')
-    elif msg.type == aiohttp.WSMsgType.ERROR:
-      print('ws connection closed with exception %s' %
-            web_socket.exception())
+	async for msg in web_socket :
+		if msg.type == aiohttp.WSMsgType.TEXT :
+			if msg.data == 'subscribe' :
+				task = loop.create_task(send_random_quote(quotes, web_socket))
+			elif msg.data == 'unsubscribe' :
+				if task:
+					task.cancel()
 
-  print('websocket connection closed')
+		elif msg.type == aiohttp.WSMsgType.ERROR :
+			print('ws connection closed with exception %s' % web_socket.exception())
 
-  return web_socket
+	print('websocket connection closed')
+
+	return web_socket
 
 
 app = web.Application()
 app.add_routes([
-    web.get('/', index),
-    web.get('/ws', websocket_handler),
+	web.get('/', index),
+	web.get('/ws', websocket_handler),
 ])
 
 if __name__ == '__main__' :
-  web.run_app(app)
+	web.run_app(app)
